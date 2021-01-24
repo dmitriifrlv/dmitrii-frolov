49019adfe07362af763dd068cdccc78c4625ec8f
Dmitrii Frolov <dmitriifrolov@Dmitriis-MacBook-Pro.local>
2021-01-25 00:41:54 +0500

    Updates

--- sw.js
+++ sw.js
@@ -27,10 +27,10 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-92d1bd979169c6b2ad56.js"
+    "url": "webpack-runtime-836d37724853c75a83c8.js"
   },
   {
-    "url": "styles.624423760e37657d5dc3.css"
+    "url": "styles.7ac02abe5776532e2ae6.css"
   },
   {
     "url": "styles-407fe62976dc5310c43e.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0d6fddf3e0f093e57b3f1db496f4c1ac"
+    "revision": "c79a47620b53e5bbc8c045b4bdf23223"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-241020a1cf7d0f317bbe.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "9e3037dfac1ccb699e8bb537d5583e11"
+    "revision": "c98516d9365dc18e0340a768f56ed5b1"
   },
   {
     "url": "polyfill-c4336065681b4417609f.js"
@@ -69,7 +69,7 @@ self.__precacheManifest = [
     "url": "dc88408d3ff7eca40f92b7844b0df82fe276bd8a-c0d8ab7bfd2529e41ff0.js"
   },
   {
-    "url": "component---src-pages-projects-js-3d7c257d3006ca38161e.js"
+    "url": "component---src-pages-projects-js-5b136de35801e75ebf20.js"
   },
   {
     "url": "page-data/projects/page-data.json",
